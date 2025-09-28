d86d98d31b28286e42c82b9e9a3f45b87926b573
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-09-28 18:38:11 +0800

    frontend tailwind and mui setup, added .env on .gitignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingScreen from "./screens/LandingScreen";
+import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingScreen />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+        <Route path="/cart" element={<ProductScreen />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
